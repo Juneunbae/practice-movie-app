844b7fcd02c5bcdfc9976ba8956725716f5db586
juneunbae <dmsqo1403@naver.com>
2026-01-06 22:09:48 +0900

    EFFECTS # useEffect

--- practice-movie-app/src/App.js
+++ practice-movie-app/src/App.js
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
     const [counter, setValue] = useState(0);
     const onClick = () => setValue((prev) => prev + 1);
-    console.log("call an api");
-  return (
-    <div>
-        <h1>{counter}</h1>
-        <button onClick={onClick}>click me</button>
-    </div>
-  );
+    console.log("i run all the time");
+
+    // useEffect function은 우리 코드가 딱 한번만 실행될 수 있도록 보호
+    useEffect(() => {
+        console.log("CALL THE API...");
+    }, []);
+
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
+        </div>
+    );
 }
 
 export default App;
