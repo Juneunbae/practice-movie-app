44b13e1363a8181144337cc4231afd062d8b1f2f
juneunbae <dmsqo1403@naver.com>
2026-01-08 21:55:38 +0900

    EFFECTS # CleanUp

--- practice-movie-app/src/App.js
+++ practice-movie-app/src/App.js
@@ -1,33 +1,29 @@
 import { useState, useEffect } from "react";
 
-function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("")
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-
-    // useEffect function은 우리 코드가 딱 한번만 실행될 수 있도록 보호
+function Hello() {
     useEffect(() => {
-        console.log("I run only once.");
+        console.log("hi :)")
+        return () => console.log("bye :(")
     }, []);
 
-    useEffect(() => {
-        console.log("I run when 'keyword' changes.")
+    useEffect(function () {
+        console.log("hi :)")
+        return function() {
+            console.log("bye :(")
+        }
+    }, []);
+    return <h1>Hello</h1>;
+}
 
-        // deps는 작성된 부분이 수정이 있을 때마다 실행하는 것을 설정
-    }, [keyword]);
-
-    useEffect(() => {
-        console.log("I run when 'counter' changes.");
-    }, [counter]);
-
+function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
     return (
         <div>
-            <input value={keyword} onChange={onChange} type={"text"} placeholder={"Search here..."} />
-            <h1>{counter}</h1>
-            <button onClick={onClick}>click me</button>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
         </div>
-    );
+    )
 }
 
 export default App;
