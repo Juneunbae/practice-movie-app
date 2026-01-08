c6ceec4179d82be39c3885d5417fe3902d9ff561
juneunbae <dmsqo1403@naver.com>
2026-01-08 21:28:20 +0900

    EFFECTS # Deps

--- practice-movie-app/src/App.js
+++ practice-movie-app/src/App.js
@@ -2,16 +2,28 @@ import { useState, useEffect } from "react";
 
 function App() {
     const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("")
     const onClick = () => setValue((prev) => prev + 1);
-    console.log("i run all the time");
+    const onChange = (event) => setKeyword(event.target.value);
 
     // useEffect function은 우리 코드가 딱 한번만 실행될 수 있도록 보호
     useEffect(() => {
-        console.log("CALL THE API...");
+        console.log("I run only once.");
     }, []);
+
+    useEffect(() => {
+        console.log("I run when 'keyword' changes.")
+
+        // deps는 작성된 부분이 수정이 있을 때마다 실행하는 것을 설정
+    }, [keyword]);
+
+    useEffect(() => {
+        console.log("I run when 'counter' changes.");
+    }, [counter]);
 
     return (
         <div>
+            <input value={keyword} onChange={onChange} type={"text"} placeholder={"Search here..."} />
             <h1>{counter}</h1>
             <button onClick={onClick}>click me</button>
         </div>
