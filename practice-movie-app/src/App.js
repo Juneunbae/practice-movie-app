748464870231ffc83130d8b0078f01a2733b1713
juneunbae <dmsqo1403@naver.com>
2026-01-11 12:28:21 +0900

    PRACTICE MOVIE APP # React Router

--- practice-movie-app/src/App.js
+++ practice-movie-app/src/App.js
@@ -1,6 +1,19 @@
+import {
+    BrowserRouter as Router
+} from "react-router-dom";
+import Switch from "react-router-dom/es/Switch";
+import Home from "./routes/Home";
+import Route from "react-router-dom/es/Route";
+import Detail from "./routes/Detail";
+
 function App() {
     // router를 render하는 역할
-    return null;
+    return <Router>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/movie" component={Detail} />
+        </Switch>
+    </Router>;
 }
 
 export default App;
--- practice-movie-app/src/component/Movie.js
+++ practice-movie-app/src/component/Movie.js
@@ -1,11 +1,14 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({coverImage, title, summary, slug, genres}) {
     return (
         <div>
             {/* eslint-disable-next-line jsx-a11y/alt-text */}
             <img src={coverImage} alt={title}/>
-            <h2>{title}</h2>
+            <h2>
+                <Link to="/movie">{title}</Link>
+            </h2>
             <p>{summary ? summary : slug}</p>
             <ul>
                 {genres.map(g => (
