9b297d207e65ef0ef884794a798501c9f90cc560
juneunbae <dmsqo1403@naver.com>
2026-01-11 10:16:58 +0900

    PRACTICE MOVIE APP # Movie App Part.1

--- practice-movie-app/src/App.js
+++ practice-movie-app/src/App.js
@@ -1,65 +1,41 @@
 import {useEffect, useState} from "react";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [coins, setCoins] = useState([]);
+    const [loading, setLoading] = useState(true)
+    const [movies, setMovies] = useState([])
+    // async-await을 사용하기 위해 getMovies 함수 생성
+    const getMovies = async () => {
+        const json = await (
+            await fetch("https://yts.lt/api/v2/list_movies.json?minimum_rating=8.8&&sort_by=year")
+        ).json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    };
     useEffect(() => {
-        fetch("https://api.coinpaprika.com/v1/tickers")
-            .then(res => res.json())
-            .then((json) => {
-                setCoins(json)
-                setLoading(false)
-            })
+        getMovies();
     }, []);
-    const [seed, setSeed] = useState("");
-    const [selectedCoin, setSelectedCoin] = useState("");
-    const [price, setPrice] = useState();
-
-    const onChangeSeed = (event) => setSeed(event.target.value);
-
-    const onSelectedCoin = (event) => {
-        const id = event.target.value;
-        setSelectedCoin(id);
-
-        const coin = coins.find((coin) => coin.id === id);
-        setPrice(coin ? coin.quotes.USD.price : null)
-    }
-
-    const amount = price != null && seed != null ? Number(seed) / Number(price) : "";
-
+    console.log(movies);
     return (
         <div>
-            <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
             {loading ? (
-                <strong>Loading...</strong>
+                <h1>Loading...</h1>
             ) : (
-                <select
-                    style={{ width: "500px", height: "30px"}}
-                    value={selectedCoin}
-                    onChange={onSelectedCoin}
-                >
-                    <option value={""} disabled>코인을 선택하세요.</option>
-                    {coins.map((coin) => (
-                        <option
-                            key={coin.id}
-                            value={coin.id}
-                        >
-                            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-                        </option>
-                    ))}
-                </select>
+                <div>
+                    {movies.map(movie =>
+                        <div key={movie.id}>
+                            {/* eslint-disable-next-line jsx-a11y/alt-text */}
+                            <img src={movie.medium_cover_image}/>
+                            <h2>{movie.title}</h2>
+                            <p>{movie.summary ? movie.summary : movie.slug}</p>
+                            <ul>
+                                {movie.genres.map(g => (
+                                    <li key={g}>{g}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
             )}
-            <br/>
-            <div>
-                <input
-                    value={seed}
-                    onChange={onChangeSeed}
-                    style={{ marginTop : "10px", width : "250px", height : "30px"}}
-                    type={"number"}
-                    placeholder={"Write your seed : $"}
-                />
-                {amount ? <span> : <strong>{amount}</strong> 개 </span> : null}
-            </div>
         </div>
     )
 }
